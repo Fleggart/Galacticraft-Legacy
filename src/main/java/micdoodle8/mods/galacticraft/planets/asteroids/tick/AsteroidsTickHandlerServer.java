af27390cfc14ca2125e8ee8c1b3e0fff2f048740
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-13 22:27:44 +0900

    Update AsteroidsTickHandlerServer.java

--- src/main/java/micdoodle8/mods/galacticraft/planets/asteroids/tick/AsteroidsTickHandlerServer.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/asteroids/tick/AsteroidsTickHandlerServer.java
@@ -7,30 +7,13 @@
 
 package micdoodle8.mods.galacticraft.planets.asteroids.tick;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
-import micdoodle8.mods.galacticraft.api.vector.BlockVec3;
-import micdoodle8.mods.galacticraft.core.entities.player.GCPlayerStats;
-import micdoodle8.mods.galacticraft.core.util.ASMUtil;
-import micdoodle8.mods.galacticraft.core.util.CompatibilityManager;
-import micdoodle8.mods.galacticraft.core.util.WorldUtil;
-import micdoodle8.mods.galacticraft.planets.GalacticraftPlanets;
 import micdoodle8.mods.galacticraft.planets.asteroids.dimension.ShortRangeTelepadHandler;
-import micdoodle8.mods.galacticraft.planets.asteroids.tile.TileEntityMinerBase;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldProvider;
-import net.minecraft.world.WorldServer;
-import net.minecraft.world.gen.ChunkProviderServer;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
-import net.minecraftforge.fml.common.gameevent.TickEvent.WorldTickEvent;
 
 public class AsteroidsTickHandlerServer
 {
@@ -55,7 +38,7 @@ public class AsteroidsTickHandlerServer
 
         if (event.phase == TickEvent.Phase.START)
         {
-            TileEntityMinerBase.checkNewMinerBases();
+            // 删除了 TileEntityMinerBase.checkNewMinerBases();
 
             if (AsteroidsTickHandlerServer.spaceRaceData == null)
             {
@@ -70,17 +53,4 @@ public class AsteroidsTickHandlerServer
             }
         }
     }
-
-    @SubscribeEvent
-    public void onWorldTick(WorldTickEvent event)
-    {
-        // No longer needed - AstroMiner removal
-    }
-
-    public static void loadAstroChunkList(List<BlockVec3> activeChunks)
-    {
-        // No longer needed - AstroMiner removal
-        activeChunks.clear();
-    }
-
 }
