308e3e2b19b87bc04660a9776db8bae79113d36b
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-13 22:19:56 +0900

    Add files via upload

--- src/main/java/micdoodle8/mods/galacticraft/planets/mars/util/RecipeUtilMars.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/mars/util/RecipeUtilMars.java
@@ -10,7 +10,6 @@ package micdoodle8.mods.galacticraft.planets.mars.util;
 import javax.annotation.Nonnull;
 import micdoodle8.mods.galacticraft.api.GalacticraftRegistry;
 import micdoodle8.mods.galacticraft.api.recipe.INasaWorkbenchRecipe;
-import micdoodle8.mods.galacticraft.planets.asteroids.inventory.InventorySchematicAstroMiner;
 import micdoodle8.mods.galacticraft.planets.asteroids.inventory.InventorySchematicTier3Rocket;
 import micdoodle8.mods.galacticraft.planets.mars.inventory.InventorySchematicCargoRocket;
 import micdoodle8.mods.galacticraft.planets.mars.inventory.InventorySchematicTier2Rocket;
@@ -53,20 +52,6 @@ public class RecipeUtilMars
         for (INasaWorkbenchRecipe recipe : GalacticraftRegistry.getRocketT3Recipes())
         {
             if (recipe.matches(inventoryRocketBench))
-            {
-                return recipe.getRecipeOutput();
-            }
-        }
-
-        return ItemStack.EMPTY;
-    }
-
-    @Nonnull
-    public static ItemStack findMatchingAstroMinerRecipe(InventorySchematicAstroMiner craftMatrix)
-    {
-        for (INasaWorkbenchRecipe recipe : GalacticraftRegistry.getAstroMinerRecipes())
-        {
-            if (recipe.matches(craftMatrix))
             {
                 return recipe.getRecipeOutput();
             }
