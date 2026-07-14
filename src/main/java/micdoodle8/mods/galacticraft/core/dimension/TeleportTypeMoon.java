6824bd8fc9ff2013fb6910e3bd3b40cc66ee99c6
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-15 07:51:35 +0900

    Add files via upload

--- src/main/java/micdoodle8/mods/galacticraft/core/dimension/TeleportTypeMoon.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/dimension/TeleportTypeMoon.java
@@ -116,10 +116,5 @@ public class TeleportTypeMoon implements ITeleportType
         }
     }
 
-    @Override
-    public void setupAdventureSpawn(EntityPlayerMP player)
-    {
-        // TODO Auto-generated method stub
-
-    }
+    // setupAdventureSpawn 方法已删除
 }
