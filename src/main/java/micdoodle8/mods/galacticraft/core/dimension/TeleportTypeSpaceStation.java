e5470b4a97c964e8143f4a0d08474597e4e6d4e0
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-15 07:54:09 +0900

    Remove setupAdventureSpawn method
    Removed the setupAdventureSpawn method from TeleportTypeSpaceStation.

--- src/main/java/micdoodle8/mods/galacticraft/core/dimension/TeleportTypeSpaceStation.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/dimension/TeleportTypeSpaceStation.java
@@ -58,10 +58,5 @@ public class TeleportTypeSpaceStation implements ITeleportType
         }
     }
 
-    @Override
-    public void setupAdventureSpawn(EntityPlayerMP player)
-    {
-        // TODO Auto-generated method stub
-
-    }
+    // setupAdventureSpawn 方法已删除
 }
