f5ad55ef9a07cb7d3b68ec1ae5a0c5ed9011adab
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-15 07:50:48 +0900

    Add files via upload

--- src/main/java/micdoodle8/mods/galacticraft/planets/mars/dimension/TeleportTypeMars.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/mars/dimension/TeleportTypeMars.java
@@ -105,10 +105,5 @@ public class TeleportTypeMars implements ITeleportType
         }
     }
 
-    @Override
-    public void setupAdventureSpawn(EntityPlayerMP player)
-    {
-        // TODO Auto-generated method stub
-
-    }
+    // setupAdventureSpawn 方法已删除
 }
