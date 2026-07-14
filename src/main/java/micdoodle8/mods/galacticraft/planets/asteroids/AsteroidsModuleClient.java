e3d603f3cc43f23d1b9c4e6ed15c6b5351daf0d1
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-14 17:31:01 +0900

    Add files via upload

--- src/main/java/micdoodle8/mods/galacticraft/planets/asteroids/AsteroidsModuleClient.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/asteroids/AsteroidsModuleClient.java
@@ -18,15 +18,18 @@ import micdoodle8.mods.galacticraft.planets.GuiIdsPlanets;
 import micdoodle8.mods.galacticraft.planets.IPlanetsModuleClient;
 import micdoodle8.mods.galacticraft.planets.asteroids.blocks.AsteroidBlocks;
 import micdoodle8.mods.galacticraft.planets.asteroids.client.FluidTexturesGC;
+// 删除: import micdoodle8.mods.galacticraft.planets.asteroids.client.fx.EntityFXTeleport;
+import micdoodle8.mods.galacticraft.planets.asteroids.client.gui.GuiShortRangeTelepad;
 import micdoodle8.mods.galacticraft.planets.asteroids.client.render.entity.RenderEntryPod;
 import micdoodle8.mods.galacticraft.planets.asteroids.client.render.entity.RenderSmallAsteroid;
 import micdoodle8.mods.galacticraft.planets.asteroids.client.render.entity.RenderTier3Rocket;
 import micdoodle8.mods.galacticraft.planets.asteroids.client.render.item.ItemModelBeamReceiver;
 import micdoodle8.mods.galacticraft.planets.asteroids.client.render.item.ItemModelBeamReflector;
 import micdoodle8.mods.galacticraft.planets.asteroids.client.render.item.ItemModelRocketT3;
-
+// 删除: import micdoodle8.mods.galacticraft.planets.asteroids.client.render.item.ItemModelTelepad;
 import micdoodle8.mods.galacticraft.planets.asteroids.client.render.tile.TileEntityBeamReceiverRenderer;
 import micdoodle8.mods.galacticraft.planets.asteroids.client.render.tile.TileEntityBeamReflectorRenderer;
+// 删除: import micdoodle8.mods.galacticraft.planets.asteroids.client.render.tile.TileEntityShortRangeTelepadRenderer;
 import micdoodle8.mods.galacticraft.planets.asteroids.entities.EntityEntryPod;
 import micdoodle8.mods.galacticraft.planets.asteroids.entities.EntitySmallAsteroid;
 import micdoodle8.mods.galacticraft.planets.asteroids.entities.EntityTier3Rocket;
@@ -34,7 +37,7 @@ import micdoodle8.mods.galacticraft.planets.asteroids.event.AsteroidsEventHandle
 import micdoodle8.mods.galacticraft.planets.asteroids.items.AsteroidsItems;
 import micdoodle8.mods.galacticraft.planets.asteroids.tile.TileEntityBeamReceiver;
 import micdoodle8.mods.galacticraft.planets.asteroids.tile.TileEntityBeamReflector;
-
+// 删除: import micdoodle8.mods.galacticraft.planets.asteroids.tile.TileEntityShortRangeTelepad;
 import micdoodle8.mods.galacticraft.planets.mars.client.fx.EntityCryoFX;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.Particle;
@@ -93,6 +96,11 @@ public class AsteroidsModuleClient implements IPlanetsModuleClient
         modelResourceLocation = new ModelResourceLocation("galacticraftplanets:beam_reflector", "inventory");
         ModelLoader.setCustomModelResourceLocation(reflector, 0, modelResourceLocation);
 
+        // 删除传送台注册
+        // Item teleporter = Item.getItemFromBlock(AsteroidBlocks.shortRangeTelepad);
+        // modelResourceLocation = new ModelResourceLocation("galacticraftplanets:telepad_short", "inventory");
+        // ModelLoader.setCustomModelResourceLocation(teleporter, 0, modelResourceLocation);
+
         modelResourceLocation = new ModelResourceLocation("galacticraftplanets:rocket_t3", "inventory");
         for (int i = 0; i < 5; ++i)
         {
@@ -108,7 +116,9 @@ public class AsteroidsModuleClient implements IPlanetsModuleClient
             "facing=up", "facing=down", "facing=north", "facing=west", "facing=east", "facing=south");
         replaceModelDefault(event, "beam_reflector", "block/reflector.obj", ImmutableList.of("Base", "Axle", "EnergyBlaster", "Ring"), ItemModelBeamReflector.class, TRSRTransformation.identity(),
             "inventory", "normal");
-
+        // 删除传送台模型注册
+        // replaceModelDefault(event, "telepad_short", "block/telepad_short.obj", ImmutableList.of("Top", "Bottom", "Connector"), ItemModelTelepad.class, TRSRTransformation.identity(), "inventory",
+        //     "normal");
         replaceModelDefault(event, "rocket_t3", "tier3rocket.obj", ImmutableList.of("Boosters", "Cube", "NoseCone", "Rocket"), ItemModelRocketT3.class, TRSRTransformation.identity());
     }
 
@@ -124,7 +134,9 @@ public class AsteroidsModuleClient implements IPlanetsModuleClient
         registerTexture(event, "minerbase");
         registerTexture(event, "beam_reflector");
         registerTexture(event, "beam_receiver");
-
+        // 删除传送台纹理
+        // registerTexture(event, "telepad_short");
+        // registerTexture(event, "telepad_short0");
         registerTexture(event, "tier3rocket");
         registerTexture(event, "space_pod");
         registerTexture(event, "fluids/argon");
@@ -158,6 +170,8 @@ public class AsteroidsModuleClient implements IPlanetsModuleClient
     {
         ClientRegistry.bindTileEntitySpecialRenderer(TileEntityBeamReflector.class, new TileEntityBeamReflectorRenderer());
         ClientRegistry.bindTileEntitySpecialRenderer(TileEntityBeamReceiver.class, new TileEntityBeamReceiverRenderer());
+        // 删除传送台渲染器绑定
+        // ClientRegistry.bindTileEntitySpecialRenderer(TileEntityShortRangeTelepad.class, new TileEntityShortRangeTelepadRenderer());
     }
 
     public static void registerBlockRenderers()
@@ -203,6 +217,13 @@ public class AsteroidsModuleClient implements IPlanetsModuleClient
     }
 
     @Override
+    public void getGuiIDs(List<Integer> idList)
+    {
+        // 删除传送台 GUI ID
+        // idList.add(GuiIdsPlanets.MACHINE_ASTEROIDS);
+    }
+
+    @Override
     public Object getGuiElement(Side side, int ID, EntityPlayer player, World world, int x, int y, int z)
     {
         TileEntity tile = world.getTileEntity(new BlockPos(x, y, z));
@@ -236,7 +257,11 @@ public class AsteroidsModuleClient implements IPlanetsModuleClient
 
             if (dX * dX + dY * dY + dZ * dZ < viewDistance * viewDistance)
             {
-
+                // 删除传送台粒子
+                // if (particleID.equals("portalBlue"))
+                // {
+                //     particle = new EntityFXTeleport(mc.world, position, motion, (TileEntityShortRangeTelepad) extraData[0], (Boolean) extraData[1]);
+                // } else 
                 if (particleID.equals("cryoFreeze"))
                 {
                     particle = new EntityCryoFX(mc.world, position, motion);
